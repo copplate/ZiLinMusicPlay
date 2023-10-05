a60cce089951e84eae4e63b41e339776badd5c6d
yuancheng <674678976@qq.com>
2023-10-05 16:28:44 +0800

    2023/10/05第一次提交

---
+++ app/src/main/java/com/example/zilinmusicplay/MusicListActivity.java
@@ -0,0 +1,14 @@
+package com.example.zilinmusicplay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MusicListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_list);
+    }
+}
